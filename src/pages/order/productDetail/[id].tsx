d2e440e85ea5866f2aa16f3250f896ed4a2c63df
Pyae Phyo Han <pyaephyohan2200@gmail.com>
2023-10-08 00:21:31 +0630

    feat: show other products at product detail

--- src/pages/order/productDetail/[id].tsx
+++ src/pages/order/productDetail/[id].tsx
@@ -5,6 +5,7 @@ import { Color, Gender, Product, Size } from "@prisma/client";
 import { useRouter } from "next/router";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import OrderAppProductCard from "@/components/OrderAppProductCard";
 
 const ProductDetail = () => {
   const router = useRouter();
@@ -31,97 +32,131 @@ const ProductDetail = () => {
   const gender = genders.find((item) => item.id === product.genderId) as Gender;
 
   return (
-    <Paper
-      sx={{
-        width: { xs: "80%", sm: "70%", md: "70%" },
-        mx: "auto",
-        display: "flex",
-        p: "1.5rem",
-        borderRadius: "2rem",
-        mt: "2rem",
-      }}>
-      <Box sx={{ width: "50%" }}>
-        <img
-          style={{ width: "100%", borderRadius: "1rem" }}
-          src={`${product.imageUrl}`}
-          alt={product.name}
-        />
-      </Box>
-      <Box sx={{ width: "50%" }}>
-        {/* header add to cart and favourite */}
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-          }}>
-          <IconButton sx={{ mr: { xs: "0.5rem", sm: "0.5rem", md: "1rem" } }}>
-            <FavoriteBorderIcon sx={{ fontSize: "2.2rem" }} color="primary" />
-          </IconButton>
+    <Box>
+      <Paper
+        sx={{
+          width: { xs: "80%", sm: "70%", md: "70%" },
+          mx: "auto",
+          display: "flex",
+          p: "1.5rem",
+          borderRadius: "2rem",
+          mt: "2rem",
+        }}>
+        <Box sx={{ width: "50%" }}>
+          <img
+            style={{ width: "100%", borderRadius: "1rem" }}
+            src={`${product.imageUrl}`}
+            alt={product.name}
+          />
+        </Box>
+        <Box sx={{ width: "50%" }}>
+          {/* header add to cart and favourite */}
           <Box
             sx={{
-              bgcolor: "primary.main",
-              width: "fit-content",
-              borderRadius: "1rem",
-              p: "0.8rem",
               display: "flex",
               alignItems: "center",
-              cursor: "pointer",
-              userSelect: "none",
+              justifyContent: "flex-end",
             }}>
-            <ShoppingCartIcon color="info" />
-            <Typography sx={{ color: "info.main", ml: "0.4rem" }}>
-              Add to cart
+            <IconButton sx={{ mr: { xs: "0.5rem", sm: "0.5rem", md: "1rem" } }}>
+              <FavoriteBorderIcon sx={{ fontSize: "2.2rem" }} color="primary" />
+            </IconButton>
+            <Box
+              sx={{
+                bgcolor: "primary.main",
+                width: "fit-content",
+                borderRadius: "1rem",
+                p: "0.8rem",
+                display: "flex",
+                alignItems: "center",
+                cursor: "pointer",
+                userSelect: "none",
+              }}>
+              <ShoppingCartIcon color="info" />
+              <Typography sx={{ color: "info.main", ml: "0.4rem" }}>
+                Add to cart
+              </Typography>
+            </Box>
+          </Box>
+          {/* product details */}
+          <Box sx={{ py: "2rem", px: { xs: "2rem", sm: "2rem", md: "3rem" } }}>
+            <Typography
+              sx={{ fontSize: { xs: "1.3rem", sm: "1.5rem", md: "1.8rem" } }}>
+              {product.name}
+            </Typography>
+            <Box sx={{ mt: "1.7rem" }}>
+              {product.discountPrice ? (
+                <Box
+                  sx={{
+                    display: "flex",
+                    fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" },
+                  }}>
+                  <Typography
+                    sx={{ textDecoration: "line-through", mr: "0.5rem" }}>
+                    {product.price} Ks
+                  </Typography>
+                  <Typography>{product.discountPrice} Ks</Typography>
+                </Box>
+              ) : (
+                <Typography
+                  sx={{ fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" } }}>
+                  Price - {product.price} Ks
+                </Typography>
+              )}
+            </Box>
+            <Typography
+              sx={{
+                fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" },
+                my: "0.5rem",
+              }}>
+              Size - {size.name}
+            </Typography>
+            <Typography
+              sx={{ fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" } }}>
+              Color - {color.name}
+            </Typography>
+            <Typography
+              sx={{
+                mt: "0.5rem",
+                fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" },
+              }}>
+              For {gender.name}
             </Typography>
           </Box>
         </Box>
-        {/* product details */}
-        <Box sx={{ py: "2rem", px: { xs: "2rem", sm: "2rem", ms: "3rem" } }}>
-          <Typography
-            sx={{ fontSize: { xs: "1.3rem", sm: "1.5rem", md: "1.8rem" } }}>
-            {product.name}
-          </Typography>
-          <Box sx={{ mt: "1.7rem" }}>
-            {product.discountPrice ? (
-              <Box
-                sx={{
-                  display: "flex",
-                  fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" },
-                }}>
-                <Typography
-                  sx={{ textDecoration: "line-through", mr: "0.5rem" }}>
-                  {product.price} Ks
-                </Typography>
-                <Typography>{product.discountPrice} Ks</Typography>
+      </Paper>
+      <Box sx={{ mt: "2rem" }}>
+        <Typography variant="h5" sx={{ textAlign: "center" }}>
+          Explore more
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: {
+              xs: "center",
+              sm: "center",
+              md: "flex-start",
+            },
+            mt: "0.5rem",
+          }}>
+          {products.map((product) => {
+            return (
+              <Box sx={{ m: "1rem" }} key={product.id}>
+                <OrderAppProductCard
+                  imageUrl={product.imageUrl as string}
+                  name={product.name}
+                  price={product.price}
+                  discountPrice={
+                    product.discountPrice ? product.discountPrice : 0
+                  }
+                  href={`/order/productDetail/${product.id}`}
+                />
               </Box>
-            ) : (
-              <Typography
-                sx={{ fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" } }}>
-                Price - {product.price} Ks
-              </Typography>
-            )}
-          </Box>
-          <Typography
-            sx={{
-              fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" },
-              my: "0.5rem",
-            }}>
-            Size - {size.name}
-          </Typography>
-          <Typography
-            sx={{ fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" } }}>
-            Color - {color.name}
-          </Typography>
-          <Typography
-            sx={{
-              mt: "0.5rem",
-              fontSize: { xs: "1rem", sm: "1.2rem", md: "1.2rem" },
-            }}>
-            For {gender.name}
-          </Typography>
+            );
+          })}
         </Box>
       </Box>
-    </Paper>
+    </Box>
   );
 };
 
