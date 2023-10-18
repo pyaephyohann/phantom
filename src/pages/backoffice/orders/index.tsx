c523ef61e545298f7157f5074ffee8b2ecbee9e9
Pyae Phyo Han <pyaephyohan2200@gmail.com>
2023-10-18 20:41:02 +0630

    fix: delete unused line

--- src/pages/backoffice/orders/index.tsx
+++ src/pages/backoffice/orders/index.tsx
@@ -34,9 +34,6 @@ const Orders = () => {
           </Typography>
         )}
       </Box>
-      <Typography sx={{ textAlign: "center" }} variant="h5">
-        You dont have any orders yet!
-      </Typography>
       {isLoading ? (
         <Box
           sx={{
